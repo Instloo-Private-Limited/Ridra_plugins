65174b99215107c793df0e744e63dc07b8fb9771
Mayank <mayankcryptobyte@gmail.com>
2024-03-05 20:17:02 +0530

    added price controll

--- Gold_price/views.py
+++ Gold_price/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import requests
 from bs4 import BeautifulSoup
+from .models import gold
 
 def get_gold_prices():
     url = "https://www.gadgets360.com/finance/gold-rate-in-india/"
@@ -22,9 +23,21 @@ def get_gold_prices():
 # Create your views here.
 def Price(request):
     gold_price = get_gold_prices()
-    context = {
-        'Price' : '{}'.format(gold_price)
-    }
+    price = int(gold_price.split(' ')[1].replace(',',''))
+    final_price = 0
+    
+    p = gold.objects.get(pk=1)
 
+    if price - p.price > 500 or price - p.price < 700:
+        final_price = gold_price
+        p.price = price
+        p.save()
+    else:
+        final_price = "₹ " + str(p.price)
+
+    
+    context = {
+        'Price' : '{}'.format(final_price)
+    }
     # return HttpResponse("{}".format(gold_price))
     return render(request, 'Price.html', context=context)
